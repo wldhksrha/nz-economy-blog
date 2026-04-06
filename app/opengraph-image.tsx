ba30612a143fe14c80b612bf4aef005415161aaa
wldhksrha <79019239+wldhksrha@users.noreply.github.com>
2026-04-06 21:44:24 +0900

    delete edge in opengraph-image.tsx

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,7 +1,6 @@
 import { ImageResponse } from 'next/og'
 import { SITE_CONFIG } from '@/lib/config'
 
-export const runtime = 'edge'
 export const alt     = SITE_CONFIG.name
 export const size    = { width: 1200, height: 630 }
 export const contentType = 'image/png'
